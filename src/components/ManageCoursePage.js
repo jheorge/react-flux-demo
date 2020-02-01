773911f96e4884b469860db806958e483efab1f7
= <jheorge@gmail.com>
2020-02-01 02:00:24 -0400

    adding promt

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { Prompt } from "react-router-dom";
 function ManageCoursePage(props) {
   return (
     <div>
       <h2>Manage course</h2>
-
+      <Prompt when={true} message="Are you sure you want to leave?" />
       {props.match.params.slug}
     </div>
   );
