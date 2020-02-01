fa1b5596ff31b614d76813b0af08732afdd402f0
= <jheorge@gmail.com>
2020-02-01 18:12:03 -0400

    adding handler to one filed in form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Prompt } from "react-router-dom";
 import CourseForm from "./CourseForm";
 
 // <Prompt when={true} message="Are you sure you want to leave?" />
 // {props.match.params.slug}
 function ManageCoursePage(props) {
+  const [course, setCourse] = useState({
+    id: null,
+    title: "",
+    slug: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleTitleChange(event) {
+    const updatedCourse = { ...course, title: event.target.value };
+    setCourse(updatedCourse);
+  }
+
   return (
     <div>
       <h2>Manage course</h2>
-      <CourseForm />
+      <CourseForm course={course} onTitleChange={handleTitleChange} />
     </div>
   );
 }
